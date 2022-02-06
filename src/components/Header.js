5c3f46b61272505f53e04686125364439aa25ba5
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2022-02-07 03:39:06 +0530

    header edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,18 @@ const Header = () => {
     <header>
       <div className="container " id="header">
         <div className="header_desc ">
-          <h1 className="hi_name"><b>Hi, I'm <span>Ripunjay</span></b></h1> 
-          <div className="titles">
-          <p className="title1">Developer</p>
-          <p className="title2">Designer</p>
-          <p className="title3">Student</p>
+          <h1 className="hi_name"><b>Hi, I'm <span>Ripunjay Narula</span></b></h1> 
+          <p className="quote" style={{marginTop:"-12px"}}><i>"You miss 100% of the shots you don't take."</i> <br/><span className="byline">-Wayne Gretzky</span>"</p>
+         <br/>
+          <div className="titles" >
+          <p className="title1" ><span className="firstletter">D</span>eveloper</p>
+          <p className="title2"><span className="firstletter">D</span>esigner</p>
+          <p className="title3"><span className="firstletter">S</span>tudent</p>
           </div>
           {/* <h2>Software Developer</h2> */}
         </div>
         <br/><br/>
-        <div className="resume_button title4">
+        <div className="resume_button title4" style={{marginTop:"-22px"}}>
               <a target="_blank" download="Resume.pdf" rel="noopener noreferrer" href={Resume}>Download Resume</a>
             </div>
       </div>
